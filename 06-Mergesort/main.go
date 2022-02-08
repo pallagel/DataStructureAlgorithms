8496a2ddb0e87b930327b55bf4fb28f68a7cde32
pallagel <lalith.pallage77@outlook.com>
2022-02-08 11:27:29 -0700

    Mergesort algorithm

---
+++ 06-Mergesort/main.go
@@ -0,0 +1,55 @@
+//Author - Lalith Pallage
+//Merge sort - First attempt
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	si := []int{1, 4, 3, 2, 10, 0, 12, 21}
+	fmt.Println("Before Sort :", si)
+	newSI := divide(si)
+	fmt.Println("After Sort :", newSI)
+}
+
+//divide Split the slice in to equal (almost) parts
+func divide(si []int) []int {
+	//return slice if length is 1
+	if len(si) == 1 {
+		return si
+	}
+
+	//split lef and right side
+	left := divide(si[:len(si)/2])
+	right := divide(si[len(si)/2:])
+
+	//call to the mergeSort funcion
+	return mergeSort(left, right)
+}
+
+//mergeSort - function to sort the split slice/array
+func mergeSort(left, right []int) []int {
+	result := make([]int, 0)
+	iPtr, jPtr := 0, 0
+
+	for iPtr < len(left) && jPtr < len(right) {
+		if left[iPtr] < right[jPtr] {
+			result = append(result, left[iPtr])
+			iPtr++
+		} else {
+			result = append(result, right[jPtr])
+			jPtr++
+		}
+	}
+
+	for ; iPtr < len(left); iPtr++ {
+		result = append(result, left[iPtr])
+	}
+	for ; jPtr < len(right); jPtr++ {
+		result = append(result, right[jPtr])
+	}
+
+	return result
+}
